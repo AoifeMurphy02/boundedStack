b2d2cc18b362e59372693653c3064f24cc63950d
aoife <aoife@10.102.211.6>
2023-03-06 12:38:14 +0000

    bounded stack with push and is full

--- BoundedStack/src/main/java/BoundedStack.java
+++ BoundedStack/src/main/java/BoundedStack.java
@@ -8,8 +8,50 @@
  * @author aoife
  */
 public class BoundedStack extends Stack {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  final int maxCapacity;
+//3. Contains two constructors (in both cases, your Stack’s constructor should be called first, using super();):
+    
+    
+    
+//a. A default constructor that sets your maxCapacity to 10
+     public BoundedStack() {
+          super();
+        this.maxCapacity = 10;
     }
+//b. A parameterized constructor that takes 1 parameter – a value for the maxCapacity.
+     //i. If the maximum capacity supplied is invalid (<= 0), throw an IllegalArgumentException  
+      public BoundedStack(int maxCapacity) {
+           super();
+           if(maxCapacity <= 0 ){
+               throw new IllegalArgumentException();
+           }
+        this.maxCapacity = maxCapacity;
+    }
+
+//    A method called push() that:
+//a. Takes 1 parameter, the String to be stored in your stack.
+//b. Checks if there is sufficient space left in the Stack to add this element
+//i. If the Stack is full, throws an IllegalArgumentException*
+//c. If sufficient space exists, uses the push method from the super class to carry out the push logic.
+    
+    @Override
+    public boolean push(String value){
+    if(!isFull()){
+        throw new IllegalArgumentException();
+    }
+    push(value);
+    return true;
+}
+//  A method called isFull() that:
+//a. Takes no parameters.
+//b. Returns true if there is space remaining in the stack.
+//c. Returns false if there is no space remaining in the stack.
+   
+    public boolean isFull(){
+        if(maxCapacity < size){
+        return false;
+    }
+        return true;
+    }
+
 }
