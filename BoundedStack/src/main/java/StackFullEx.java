dd003232e7489c762d7db692562cc814b3de4831
aoife <aoife@10.102.211.6>
2023-03-08 12:19:45 +0000

    stackFullEx

---
+++ BoundedStack/src/main/java/StackFullEx.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author aoife
+ */
+public class StackFullEx extends RuntimeException{
+
+    public StackFullEx() {
+    }
+
+    public StackFullEx(String string) {
+        super(string);
+    }
+    
+}
